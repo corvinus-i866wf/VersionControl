9a5d708a47d6214cb780ba6b68b855641051b97e
Gergely Masek <gergely.masek@stud.uni-corvinus.hu>
2021-09-30 19:12:59 +0200

    6-ospont

--- Use maintanence/Form1.cs
+++ Use maintanence/Form1.cs
@@ -12,9 +12,28 @@ namespace Use_maintanence
 {
     public partial class Form1 : Form
     {
+        BindingList<Entities.User> users = new BindingList<Entities.User>();
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.FirstName;
+            label2.Text = Resource1.LastName;
+            button1.Text = Resource1.Add;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new Entities.User()
+            {
+                LastName = textBox2.Text,
+                FirstName = textBox1.Text
+            };
+            users.Add(u);
         }
     }
 }
