20df7e4ca02ada0dcaee30a1bdb0d478c7938723
Gergely Masek <gergely.masek@stud.uni-corvinus.hu>
2021-10-08 10:47:45 +0200

    excel megnyit

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -18,17 +18,46 @@ namespace WindowsFormsApp1
         RealEstateEntities context = new RealEstateEntities();
 
         List<Flat> lakasok;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
 
         public Form1()
         {
             InitializeComponent();
             LoadData();
             dataGridView1.DataSource = lakasok;
-            
+            CreateExcel();
         }
         public void LoadData()
         {
             lakasok = context.Flats.ToList();
+        }
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+                CreateTable();
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string hiba = string.Format("Error:¸{0}\nLine:{1}", ex.Message, ex.Source);
+                xlWB.Close(false,Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+            
+        }
+        private void CreateTable()
+        {
+
         }
     }
 }
