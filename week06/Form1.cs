0cb033860f091254ce3a686cd1b2877d67ff3c77
Gergely Masek <gergely.masek@stud.uni-corvinus.hu>
2021-10-29 20:14:39 +0200

    kesz

--- week06/Form1.cs
+++ week06/Form1.cs
@@ -7,14 +7,123 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using week06.MnbServiceReference;
+using System.Xml;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace week06
 {
     public partial class Form1 : Form
     {
+        BindingList<Entitites.RateData> Rates = new BindingList<Entitites.RateData>();
+        string result;
+        BindingList<string> Currencies = new BindingList<string>();
+        
+        
         public Form1()
         {
             InitializeComponent();
+            GetCurr();
+            RefreshData();
+        }
+        public void RefreshData()
+        {
+            Rates.Clear();
+            Webhiv();
+            XmlFeldolgoz();
+            DiagramJelenit();
+            comboBox1.DataSource = Currencies;
+            dataGridView1.DataSource = Rates;
+
+        }
+        public void GetCurr()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetCurrenciesRequestBody();
+            var response = mnbService.GetCurrencies(request);
+            var result = response.GetCurrenciesResult;
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                for (int i = 0; i < element.ChildNodes.Count; i++)
+                {
+                    var x = (XmlElement)element.ChildNodes[i];
+                    Currencies.Add(x.InnerText);
+                }
+
+            }
+            
+
+        }
+      
+        public void Webhiv()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            result = response.GetExchangeRatesResult;
+        }
+        public void XmlFeldolgoz()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new Entitites.RateData();
+                Rates.Add(rate);
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+
+
+            }
+        }
+        public void  DiagramJelenit()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
